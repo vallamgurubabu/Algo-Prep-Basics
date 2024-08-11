dff419608d1668621bf02a9fff7641eb13cd4507
gurubabu vallam <vallamgurubabu@gmail.com>
2024-08-11 17:38:00 +0530

    commit Strings

---
+++ Practice/String_middle.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class String_middle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str =  sc.nextLine();
+        int n = str.length();
+        int div = 0;
+        if(n%2==0){
+            div = n/2;
+        }else{
+            div = (n+1)/2;
+        }
+        String ans = ""; 
+        int spaces = n-2;
+        for(int i=div-1;i<n;i++){
+            for(int j =spaces;j>=0;j--){
+              System.out.print(" ");
+            }
+            ans+=str.charAt(i);
+            System.out.println(ans);
+            spaces--;
+        }
+        for(int i=0;i<div-1;i++){
+            for(int j =spaces;j>=0;j--){
+                System.out.print(" ");
+              }
+            ans+=str.charAt(i);
+            System.out.println(ans);
+            spaces--;
+        }
+    }
+}
