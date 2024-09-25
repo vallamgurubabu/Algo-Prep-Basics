8b1fea70bd75c2ef9d3a65a4dcfe3b0bc6eb4f6f
gurubabu vallam <vallamgurubabu@gmail.com>
2024-09-25 14:01:16 +0530

    commit

---
+++ Practice/Strings/Test.java
@@ -0,0 +1,39 @@
+package Strings;
+
+import java.util.*;
+class Test{
+    public static void main(String[] args) {
+        String [] words = {"abnbnb","apple","mango","banana","guva"};
+        HashMap<String,Integer> ans = new HashMap<>();
+        int len = words.length;
+        for(int i=0;i<len;i++){
+           ans.put(words[i],distinctCount(words[i]));
+        }
+        int min = Integer.MAX_VALUE;
+        String an ="";
+        for(String val : ans.keySet()){
+            if(ans.get(val)<min){
+                min = ans.get(val);
+                an = val;
+            }
+            System.err.println(val+" "+ans.get(val));
+            
+        }
+        System.out.println(an);
+
+    }
+    public static int distinctCount(String str){
+        int count =0;
+        HashMap<Character,Integer> hs = new HashMap<>();
+        int len = str.length();
+        for(int i=0;i<len;i++){
+            hs.put(str.charAt(i),hs.getOrDefault(str.charAt(i),0 )+1);
+        }
+        for(char ch : hs.keySet()){
+            if(hs.get(ch)==1){
+                count++;
+            }
+        }
+        return count;
+    }
+}
